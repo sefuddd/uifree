f373c40e811f537d5a829dae4baa0b801d1b3b15
sefuddd <54879573+sefuddd@users.noreply.github.com>
2024-11-01 18:19:40 -0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -2115,7 +2115,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 			Toggle.Name = ToggleSettings.Name
 			Toggle.Title.Text = ToggleSettings.Name
 			-- teste
-			Toggle.Title.Position = UDim2.new(0, 15, 0, 1)
+			--Toggle.Title.Position = UDim2.new(0, 15, 0, 1)
 			print(Toggle.Title.Position)
 			Toggle.Visible = true
 			Toggle.Parent = TabPage
@@ -2207,6 +2207,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 				if NewToggleValue then
 					ToggleSettings.CurrentValue = true
 					TweenService:Create(Toggle, TweenInfo.new(0.6, Enum.EasingStyle.Quint), {BackgroundColor3 = SelectedTheme.ElementBackgroundHover}):Play()
+					TweenService:Create(Toggle.Title.Position(0.7, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 15, 0, 1)}):Play()
 					TweenService:Create(Toggle.UIStroke, TweenInfo.new(0.6, Enum.EasingStyle.Quint), {Transparency = 1}):Play()
 					TweenService:Create(Toggle.Switch.Indicator, TweenInfo.new(0.5, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {Position = UDim2.new(1, -20, 0.5, 0)}):Play()
 					TweenService:Create(Toggle.Switch.Indicator, TweenInfo.new(0.4, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {Size = UDim2.new(0,12,0,12)}):Play()
@@ -2416,7 +2417,6 @@ function RayfieldLibrary:CreateWindow(Settings)
 	TweenService:Create(Main, TweenInfo.new(0.7, Enum.EasingStyle.Quint), {Size = useMobileSizing and UDim2.new(0, 500, 0, 275) or UDim2.new(0, 650, 0, 475)}):Play()
 	TweenService:Create(Elements, TweenInfo.new(0.8, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 590, 1, -100)}):Play()
 	TweenService:Create(Main.Topbar, TweenInfo.new(0.5, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 650, 0, 45)}):Play()
-	TweenService:Create(Elements.Template.Toggle.Title.Position(0.7, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 15, 0, 1)}):Play()
 	TweenService:Create(Main.Shadow.Image, TweenInfo.new(0.7, Enum.EasingStyle.Quint), {ImageTransparency = 0.4}):Play()
 
 	Topbar.BackgroundTransparency = 1
