6c9e5d9853a3873575930dae5be4f99ad057e3ec
sefuddd <54879573+sefuddd@users.noreply.github.com>
2024-11-01 14:29:33 -0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1169,7 +1169,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 		TabButton.Title.Text = Name
 		TabButton.Parent = TabList
 		TabButton.Title.TextWrapped = false
-		TabButton.Size = UDim2.new(0, TabButton.Title.TextBounds.X + 30, 0, 30)
+		TabButton.Size = UDim2.new(0, TabButton.Title.TextBounds.X + 300, 0, 30)
 
 		if Image then
 			TabButton.Title.AnchorPoint = Vector2.new(0, 0.5)
@@ -1285,8 +1285,6 @@ function RayfieldLibrary:CreateWindow(Settings)
 			Button.Title.TextTransparency = 1
 
 			TweenService:Create(Button, TweenInfo.new(0.7, Enum.EasingStyle.Quint), {BackgroundTransparency = 0}):Play()
-			--caraio
-			--TweenService:Create(Button, TweenInfo.new(1, Enum.EasingStyle.Quint), {Size = UDim2.new(0, 460, 1, 45)}):Play()
 			TweenService:Create(Button.UIStroke, TweenInfo.new(0.7, Enum.EasingStyle.Quint), {Transparency = 0}):Play()
 			TweenService:Create(Button.Title, TweenInfo.new(0.7, Enum.EasingStyle.Quint), {TextTransparency = 0}):Play()	
 
