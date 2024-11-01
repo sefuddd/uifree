38f6813ca77c93764cd3b61bdcae9a783a5db316
sefuddd <54879573+sefuddd@users.noreply.github.com>
2024-11-01 20:31:45 -0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1756,7 +1756,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 
 			Dropdown.Size = UDim2.new(1, -10, 0, 45)
 			-- teste
-			Dropdown.Toggle = Udim2.new(0, 40, 0, 40)
+			--Dropdown.Toggle = Udim2.new(0, 40, 0, 40)
 
 			TweenService:Create(Dropdown, TweenInfo.new(0.7, Enum.EasingStyle.Quint), {BackgroundTransparency = 0}):Play()
 			TweenService:Create(Dropdown.UIStroke, TweenInfo.new(0.7, Enum.EasingStyle.Quint), {Transparency = 0}):Play()
